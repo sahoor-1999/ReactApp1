d3c161fbf06799e155c3279ff27df93fd331c1f8
sahoor-1999 <sahooratnakar666@gmail.com>
2024-06-25 16:23:55 +0530

    more logic added to the textUtils

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,56 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-
-export default function Navbar({title='set title', aboutUs='set about us', home='set home'}) {
+export default function Navbar({
+  title = "set title",
+  aboutUs = "set about us",
+  home = "set home",
+}) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">{title}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">{home}</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" href="/">{aboutUs}</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-  )
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          {title}
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                {home}
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="/">
+                {aboutUs}
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-warning" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 /** SYNTAX FOR PROP-TYPE
@@ -36,7 +60,7 @@ export default function Navbar({title='set title', aboutUs='set about us', home=
  *                        }
  * }
  */
-Navbar.propTypes={
-                title:PropTypes.string.isRequired,
-                aboutUs:PropTypes.string.isRequired
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  aboutUs: PropTypes.string.isRequired,
 };
